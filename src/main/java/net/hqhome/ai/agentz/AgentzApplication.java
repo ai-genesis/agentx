e0d62093e613037406b7efb0745a20faeec626f8
HQidea <hqidea@yahoo.com>
2024-03-17 15:44:52 +0800

    feat: init spring

---
+++ src/main/java/net/hqhome/ai/agentz/AgentzApplication.java
@@ -0,0 +1,13 @@
+package net.hqhome.ai.agentz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/hqhome/ai/agentz/AgentzApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hqhome.ai.agentz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
